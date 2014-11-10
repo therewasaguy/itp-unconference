84acd228c8d6042c2b4802e52a0a593d0bcde495
sslover <samjslover@gmail.com>
2014-11-10 13:27:53 -0500

    saving to db

--- models/model.js
+++ models/model.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 // define a schema
 var TopicSchema = new Schema({
 	description: String,
-	type: {type:String, enum:'teach vote'.split(' ')},
+	type: {type:String, enum:'teach learn'.split(' ')},
 	voteCode: String,
 	voteCount: Number,
 	person: String, // if proposing to teach it, we need their name
